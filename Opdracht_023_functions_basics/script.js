d06abd10d474260ecd0b696d2f5145a7e5206f41
Rob <78263794+RWMHermens@users.noreply.github.com>
2021-04-16 15:46:20 +0200

    JS Function Basics
    Making Pizza & Sushi!

---
+++ Opdracht_023_functions_basics/script.js
@@ -0,0 +1,19 @@
+function makePizza() {
+    console.log('Get pizza out of freezer');
+    console.log('Add extra paprika');
+    console.log('Add pepper and oregano');
+}
+
+function makeSushi() {
+    console.log('Cook rice');
+    console.log('Put rice in seaweed');
+    console.log('Add a bit of salmon');
+    console.log('Roll the sushiroll');
+}
+
+makeSushi();
+makeSushi();
+makeSushi();
+
+makePizza();
+makePizza();
